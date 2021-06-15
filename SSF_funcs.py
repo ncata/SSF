b5371b7b59c759ec0a86eddb312e563362435a54
Nick Catanzaro <ncatanzaro1@gmail.com>
2021-06-15 19:03:15 -0400

    Images and code updates

--- SSF_funcs.py
+++ SSF_funcs.py
@@ -1,6 +1,6 @@
 # import libraries
 import matplotlib.pyplot as plt
-from PIL import Image
+import cv2
 import numpy as np
 from skimage.feature import register_translation
 
@@ -8,8 +8,8 @@ from skimage.feature import register_translation
 def translateimage(i1, i2):
     ''' Takes an input of two images and registers the translation between them. Then translates the second image to better match the first one '''
     # read images and convert to grayscale
-    im1 = np.asarray(Image.open(i1).convert("L"))
-    im2 = np.asarray(Image.open(i2).convert("L"))
+    im1 = np.asarray(cv2.imread(i1, 0))
+    im2 = np.asarray(cv2.imread(i2, 0))
 
     # Find the x,y shift
     shift, error, diffphase = register_translation(im1, im2)
@@ -18,18 +18,20 @@ def translateimage(i1, i2):
     # shift the second image to align with the first
     y, x = np.shape(im1)
     print (type(x), type(y))
-    iy, ix = shift #change to int so it will work please thanks.
+    iy, ix = shift
     print (type(ix), type(iy))
-    im2reg = im2[y + iy, x + ix]
+    trans_matrix = np.float32([[1,0,iy], [0,1,ix]])
+    rows, cols = im2.shape
+    im2reg = cv2.warpAffine(im2, trans_matrix, (cols,rows))
 
     # plot images
     plt.subplot(221)
     plt.imshow(im1,cmap='Greys')
     plt.title('Reference')
-    plt.subplot(223)
+    plt.subplot(222)
     plt.imshow(im2,cmap='Greys')
     plt.title('Shifted')
-    plt.subplot(222)
+    plt.subplot(223)
     plt.title('Aligned')
     plt.imshow(im2reg,cmap='Greys')
 
